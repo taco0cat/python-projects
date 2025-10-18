9e4fac33858d8998b410389a9323619b8ed9444c
taco0cat <hashirhameed107@gmail.com>
2025-10-18 16:00:08 +0400

    Caesar Cipher Updated

--- 01 - Caesar_Cipher.py
+++ 01 - Caesar_Cipher.py
@@ -1,18 +1,26 @@
 def caesar(text, shift, encrypt=True):
 
+    # Validating only integers for shift variable
     if not isinstance(shift, int):
         return 'Shift must be an integer value.'
 
+    # Validating shift between 1 and 25
     if shift < 1 or shift > 25:
         return 'Shift must be an integer between 1 and 25.'
 
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+    # If encrypt is false, shift will be negative
     if not encrypt:
-        shift = - shift
+        shift = -shift
     
+    # Shifting alphabet based on parameter given
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+
+    # Creating translation table using str.maketrans(a, b) # A is mapped to B
     translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    
+    # Encrypting/decrypting text
     encrypted_text = text.translate(translation_table)
     return encrypted_text
 
@@ -21,8 +29,11 @@ def encrypt(text, shift):
     
 def decrypt(text, shift):
     return caesar(text, shift, encrypt=False)
+
+# Example
 # encrypted_text = encrypt('freeCodeCamp', 3)
+# print(encrypted_text)
+
 encrypted_text = "Pbhentr vf sbhaq va hayvxryl cynprf."
 decrypted_text = decrypt(encrypted_text, 13)
-# print(encrypted_text)
 print(decrypted_text)
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Projects
+
+## 01 - Encryption / Decryption using a Caesar Cipher
+
+#### What is a a Caesar Cipher?
+    - It's a type of substitution cipher where each letter is shifted a certain number of places down or up the alphabet.
+
+#### Is it safe and how do you crack it?
+    - The main weakness is that there are only 25 possible shifts.
+    - Easy to crack using **Brute-Force** or **Frequency **Analysis**
+
+## Future Updates
+- User input
+- Special Characters (@, #, $...)
